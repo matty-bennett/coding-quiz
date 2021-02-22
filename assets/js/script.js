8faa8a0d402d71aa074f6f193560b3a263e496d7
matty-bennett <bennyot10@gmail.com>
2021-02-21 23:45:39 -0700

    added html and css to start game and some psuedocode for js

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,67 @@ function countdown() {
 // start timer when user clicks start quiz button
 
 // start quiz when user clicks start button
+const question = document.querySelector("#question");
+const choices = Arry.from(document.querySelectorAll(".choice-text"));
+
+let currentQuestion = {};
+let answers = true;
+let questionArr = [];
+let questionNum = 0;
+
+let questions = [
+    {
+        question: 'This will be question 1',
+        choice1: 'This will be choice 1',
+        choice2: 'This will be choice 2',
+        choice3: 'This will be choice 3',
+        choice4: 'This will be choice 4',
+        answer: 1,
+    },
+    {
+        question: 'This will be question 1',
+        choice1: 'This will be choice 1',
+        choice2: 'This will be choice 2',
+        choice3: 'This will be choice 3',
+        choice4: 'This will be choice 4',
+        answer: 1,
+    },
+    {
+        question: 'This will be question 1',
+        choice1: 'This will be choice 1',
+        choice2: 'This will be choice 2',
+        choice3: 'This will be choice 3',
+        choice4: 'This will be choice 4',
+        answer: 1,
+    },
+    {
+        question: 'This will be question 1',
+        choice1: 'This will be choice 1',
+        choice2: 'This will be choice 2',
+        choice3: 'This will be choice 3',
+        choice4: 'This will be choice 4',
+        answer: 1,
+    },
+    {
+        question: 'This will be question 1',
+        choice1: 'This will be choice 1',
+        choice2: 'This will be choice 2',
+        choice3: 'This will be choice 3',
+        choice4: 'This will be choice 4',
+        answer: 1,
+    },
+    {
+        question: 'This will be question 1',
+        choice1: 'This will be choice 1',
+        choice2: 'This will be choice 2',
+        choice3: 'This will be choice 3',
+        choice4: 'This will be choice 4',
+        answer: 1,
+    }
+]
+
+// function for accessing and iterating over question array
+
+// add click events choice questions and function to respond
+
+// function for getting and saving high scores
